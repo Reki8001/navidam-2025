2b02d15bbd9934d90f587be09fa9bc4fad20b5ab
Raúl <raulgarcia6345@gmail.com>
2025-12-18 09:51:03 +0100

    Maracuya

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,9 +1,11 @@
 import {createBrowserRouter} from "react-router-dom";
 import PageNotFound from "@/pages/PageNotFound.tsx";
 import Postal from "@/pages/Postal.tsx";
+import Home from "@/pages/Home";
 
 
 export const router = createBrowserRouter([
+    {path: "/", element: <Home/>},
     {path: "/postal", element: <Postal/>},
     {path: "*", element: <PageNotFound/>}
 
