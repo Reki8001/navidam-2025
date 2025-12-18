a75511dbcc31bbe84663b74ffdd11cf0b84f15ef
JuanTrujillo20 <juantrujillom19@gmail.com>
2025-12-18 11:02:10 +0100

    creado el dto y el controllers

---
+++ backend/api/src/main/java/com/navidam/api/web/controller/PersonaController.java
@@ -0,0 +1,19 @@
+package com.navidam.api.web.controller;
+
+import com.navidam.api.service.PersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class PersonaController {
+
+    private final PersonaService personaService;
+
+    @Autowired
+    public PersonaController(PersonaService personaService){this.personaService = personaService;}
+
+}   public List<PersonaResponse> getPersonas(){
+    
+}
