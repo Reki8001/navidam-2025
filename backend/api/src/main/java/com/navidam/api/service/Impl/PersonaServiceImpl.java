674be63801c5486e9c1ce3c1a65a057d4f91bc91
JuanTrujillo20 <juantrujillom19@gmail.com>
2025-12-18 12:27:55 +0100

    solucionado el error en el service de la anotacion Overridde

--- backend/api/src/main/java/com/navidam/api/service/Impl/PersonaServiceImpl.java
+++ backend/api/src/main/java/com/navidam/api/service/Impl/PersonaServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class PersonaServiceImpl {
+public class PersonaServiceImpl  implements PersonaService {
     private final PersonaRepository personaRepository;
 
     @Autowired
@@ -18,10 +18,20 @@ public class PersonaServiceImpl {
     }
 
     @Override
-    public List<PersonaResponse>listaPersonas(){
+    public List<PersonaResponse> listaPersonas(){
         return personaRepository.findAll().stream()
                 .map(p -> new PersonaResponse(p.getId(), p.getNombre(), p.getEmail()))
                 .toList();
+    }
+
+    @Override
+    public PersonaResponse getPersona(Long id) {
+        return null;
+    }
+
+    @Override
+    public PersonaResponse getPersonaEmail(String email) {
+        return null;
     }
 
     @Override
