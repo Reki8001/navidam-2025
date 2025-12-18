18b67d0e439bedc1963e6823e8429fbed4ca05ee
JuanTrujillo20 <juantrujillom19@gmail.com>
2025-12-18 09:45:41 +0100

    creada la page postal

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,7 +1,10 @@
 import {createBrowserRouter} from "react-router-dom";
 import PageNotFound from "@/pages/PageNotFound.tsx";
+import Postal from "@/pages/Postal.tsx";
 
 
 export const router = createBrowserRouter([
-    {path: "*", element: <PageNotFound/>},
+    {path: "/postal", element: <Postal/>},
+    {path: "*", element: <PageNotFound/>}
+
 ]);
